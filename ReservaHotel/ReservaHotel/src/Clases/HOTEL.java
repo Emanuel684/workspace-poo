a3ade0a199915e29864703990691e360c6b11375
emanuel.acevedo <emanuel.acevedo@quipux.com>
2022-11-06 18:20:43 -0500

    Agrega la funcionalidad de la ultima parte de toda la app

--- ReservaHotel/ReservaHotel/src/Clases/HOTEL.java
+++ ReservaHotel/ReservaHotel/src/Clases/HOTEL.java
@@ -30,6 +30,49 @@ public class HOTEL {
 		System.out.println("yearInicio: " + diaFinal);
 		System.out.println("yearInicio: " + mesFinal);
 		System.out.println("yearInicio: " + yearFinal);
+		
+		String gananciasTotales = "0";
+		
+		for (int j = 0; j < INICIO.datosReservas.length; j++) {
+			
+			String diaInicioReserva = INICIO.datosReservas[j][0].split("/")[0];
+			String mesInicioReserva = INICIO.datosReservas[j][0].split("/")[1];
+			String yearInicioReserva = INICIO.datosReservas[j][0].split("/")[2];
+			
+			if(Integer.parseInt(diaInicio) < Integer.parseInt(diaInicioReserva) && Integer.parseInt(diaInicioReserva) < Integer.parseInt(diaFinal)) {
+				
+				if(Integer.parseInt(mesInicio) < Integer.parseInt(mesInicioReserva) && Integer.parseInt(mesInicioReserva) < Integer.parseInt(mesFinal)) {
+					
+					if(Integer.parseInt(yearInicio) < Integer.parseInt(yearInicioReserva) && Integer.parseInt(yearInicioReserva) < Integer.parseInt(yearFinal)) {
+						System.out.println("CUMPLE CON LA FECHA");
+						System.out.println("INICIO.datosReservas[j][0]Dia: " + INICIO.datosReservas[j][0].split("/")[0]);
+						System.out.println("INICIO.datosReservas[j][0]Mes: " + INICIO.datosReservas[j][0].split("/")[1]);
+						System.out.println("INICIO.datosReservas[j][0]Año: " + INICIO.datosReservas[j][0].split("/")[2]);
+					}
+					
+				}
+				
+			}
+			
+			System.out.println("INICIO.datosReservas[j][0]Dia: " + INICIO.datosReservas[j][0].split("/")[0]);
+			System.out.println("INICIO.datosReservas[j][0]Mes: " + INICIO.datosReservas[j][0].split("/")[1]);
+			System.out.println("INICIO.datosReservas[j][0]Año: " + INICIO.datosReservas[j][0].split("/")[2]);
+				
+			/*
+				INICIO.datosReservas[j][0] = fechaInicio;
+				INICIO.datosReservas[j][1] = cantidadDias;
+				INICIO.datosReservas[j][2] = cedulaCliente;
+				INICIO.datosReservas[j][3] = numeroHabitacion;
+				INICIO.datosReservas[j][4] = precioHabitacion;
+				INICIO.datosReservas[j][5] = String.valueOf(j);
+				*/
+			gananciasTotales = String.valueOf(Integer.parseInt(gananciasTotales) + Integer.parseInt(INICIO.datosReservas[j][4]));
+			
+			System.out.println("INICIO.datosReservas[j][4]: " + INICIO.datosReservas[j][4]);
+			System.out.println("gananciasTotales: " + gananciasTotales);
+			
+		}
+		
 	}
 
 }
